364dadb44504a7a1385cbb4478780cc3a754ce0b
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2026-03-24 11:53:21 +0100

    Remove replace truck delivery - greedy is very slow and not high-reward

--- truck_and_drone/src/main.cpp
+++ truck_and_drone/src/main.cpp
@@ -15,7 +15,6 @@ int main()
     run_gam(
         {
             NamedOperator{"1-reinsert", one_reinsert_random},
-            NamedOperator{"Replace Truck Delivery", replace_truck_delivery_greedy},
             NamedOperator{"Two-Opt", two_opt_random},
             NamedOperator{"NN-Reassign", nearest_neighbour_reassign_random},
         }
